fb4d89f713e527d619592b3eed75d9b387bb854a
max graves <max graves>
2017-03-14 16:57:02 -0400

    binary shadow test

--- Parallel/balls/main.c
+++ Parallel/balls/main.c
@@ -99,14 +99,50 @@ void vAndNormalize(point* a, point* b, vector* c)
 }
 int rgb[M][N][3] ;
 
+point p;
+point eToC;
+vector sight;
+
+int isShadowed(point* target)
+{
+    int i;
+    double b;
+    double c;
+    double minT = 10000000;
+    for(i = 0; i<4; i++)
+    {
+
+        eToC.x = e.x-s[i].c.x;
+        eToC.y = e.y-s[i].c.y;
+        eToC.z = e.z-s[i].c.z;
+        b = 2*(sight.r.x*(eToC.x) + sight.r.y*(eToC.y) + sight.r.z*(eToC.z));
+        c = (eToC.x*eToC.x) + (eToC.y*eToC.y) + (eToC.z*eToC.z)- (s[i].r*s[i].r);
+        double disc = (b*b)-4*c;
+        if (disc >= 0)
+        {
+            double T1 = (-b + sqrt(disc))/2;
+            if(T1 > 0 && T1 < minT)
+            {
+                return 1;
+            }
+
+            double T2 = (-b - sqrt(disc))/2;
+            if(T2 > 0 && T2 < minT)
+            {
+                return 1;
+            }
+        }
+    }
+}
 //
+point p;
+point eToC;
+vector sight;
+
 int main(void)
 {
     init();
     int y, x ;
-    point p;
-    point eToC;
-    vector sight;
     for( y = 0 ; y < N ; y++ )
     {
         for( x = 0 ; x < M ; x++)
@@ -149,25 +185,35 @@ int main(void)
                     }
                 }
             }
+            p.x = sight.s.x + minT*0.9999999*sight.r.x;
+            p.y = sight.s.y + minT*0.9999999*sight.r.y;
+            p.z = sight.s.z + minT*0.9999999*sight.r.z;
+            if( isShadowed(&p) == 1)
+            {
+                rgb[x][y][0] = s[i].h.r*0.5;
+                rgb[x][y][1] = s[i].h.g*0.5;
+                rgb[x][y][2] = s[i].h.b*0.5;
+            }
+
 
         }
     }
     FILE* fout ;
-fout = fopen( "balls.ppm" , "w" ) ;
-   //
-   fprintf( fout , "P3\n" ) ;
-   fprintf( fout , "%d %d\n" , M , N ) ;
-   fprintf( fout , "255\n" ) ;
-   //
-   for( y = 0 ; y < N ; y++ )
-   {
-      for( x = 0 ; x < M ; x++)
-      {
-         fprintf( fout , "%d %d %d\n" ,
-          rgb[y][x][0] , rgb[y][x][1] , rgb[y][x][2] ) ;
-      }
-   }
-   fclose( fout ) ;
-   //
-   return 0 ;
+    fout = fopen( "balls.ppm", "w" ) ;
+    //
+    fprintf( fout, "P3\n" ) ;
+    fprintf( fout, "%d %d\n", M, N ) ;
+    fprintf( fout, "255\n" ) ;
+    //
+    for( y = 0 ; y < N ; y++ )
+    {
+        for( x = 0 ; x < M ; x++)
+        {
+            fprintf( fout, "%d %d %d\n",
+                     rgb[y][x][0], rgb[y][x][1], rgb[y][x][2] ) ;
+        }
+    }
+    fclose( fout ) ;
+    //
+    return 0 ;
 }
